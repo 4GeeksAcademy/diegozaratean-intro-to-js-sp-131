35239bc072532ae3ffe0db391b988d81878634ae
diego <diegozaratean@gmail.com>
2026-01-19 18:58:59 +0000

    finish js class 1

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log('hola desde main js')
+
+
+let nombre = 'francisco'
+let apellido = 'guerra'
+let edad = 35
+let estaVivo = false
+const pi =3.14
+
+const nocambio = edad + pi
+
+
+//    elemento.  1.        2.          3
+let frutas = ['banana','parchita','tomate']
+// posición.     0.        1.          2
+// length = 3
+console.log(frutas)
+console.log(frutas[1])
+
+
+let coche = {marca:'audi',modelo:'rs3',precio:80000,disponible: true}
