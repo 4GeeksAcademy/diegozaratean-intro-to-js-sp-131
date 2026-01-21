3a8ac495fed6c039cec90119e5a12dbfc3ea325b
diego <diegozaratean@gmail.com>
2026-01-21 18:55:49 +0000

    finish for class

--- main.js
+++ main.js
@@ -1,21 +1,98 @@
 console.log('hola desde main js')
 
-
-let nombre = 'francisco'
-let apellido = 'guerra'
-let edad = 35
-let estaVivo = false
-const pi =3.14
-
-const nocambio = edad + pi
+console.log("manual")
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
 
 
-//    elemento.  1.        2.          3
-let frutas = ['banana','parchita','tomate']
-// posición.     0.        1.          2
-// length = 3
+console.log("con un bucle")
+// i = 1 ===> 1
+// i = 2 ===> 2
+// i = 3 ===> 3
+// i = 4 ===> 4
+// i = 5 ===> 5
+// i = 6 ===> 
+// for(inicialización;condición;Actualización)
+let maximo = 6
+for(let i = 1;i < maximo ; i++){
+    console.log(i)
+}
+
+
+
+
+
+
+
+
+
+// i = i -1 ====> i--
+// i = i +1 ====> i++
+// i = i +x ====> i+=x
+
+for(let i = 20; i >= 15; i-- ){
+    console.log(i)
+}
+
+for(j =30; j  <= 50; j+=5){
+    console.log(j)
+}
+
+// ele.           1.      2.      3
+let nombres = ['pablo','mario','karla']
+// pos.           0.       1.     2. 
+console.log(nombres)
+console.log(nombres[2])
+console.log(nombres[0])
+
+console.log(nombres)
+nombres.push('agustin')
+console.log(nombres)
+
+const alumnos = ['ana','zoe','patricia']
+console.log(alumnos)
+alumnos.push('agustin')
+console.log(alumnos)
+
+
+
+const frutas = ['piña','kiwi','arandano','banana']
+
 console.log(frutas)
 console.log(frutas[1])
 
+frutas.push('melon')
+console.log(frutas)
 
-let coche = {marca:'audi',modelo:'rs3',precio:80000,disponible: true}
+frutas.unshift('mango')
+console.log(frutas)
+console.log(frutas[1])
+
+frutas.pop()
+console.log(frutas)
+
+frutas.shift()
+console.log(frutas)
+
+
+// frutas.splice(1,1)
+// console.log(frutas)
+// frutas.splice(2,1)
+// console.log(frutas)
+
+frutas.splice(1,4,'tomate','pera','durazno')
+console.log(frutas)
+// frutas.splice(1,0,'melocoton')
+// console.log(frutas)
+
+
+console.log(frutas[0] + "s")
+console.log(frutas[1] + 's')
+console.log(frutas[2] + "s")
+
+for(let i = frutas.length - 1 ; i >= 0 ;i--){
+    console.log(frutas[i] + 's')
+}
