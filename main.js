6e70cb1e6d2ab1767d91dc0b9c8216a02b947e42
diego <diegozaratean@gmail.com>
2026-01-26 19:36:28 +0000

    finish loop class

--- main.js
+++ main.js
@@ -1,115 +1,113 @@
 console.log('hola desde main js')
 
-// let nombre = "Lauro"
-// console.log('Antes de la funcion' + nombre)
-// function saludarAlumno(nombre){
-//     console.log('dentro de la funcion' + nombre)
-//     console.log("hola " + nombre)
-// }
-// console.log('Despues de la funcion' + nombre)
+// Array
+// Mario
+// Luigi
+// Bowser
+// Toad
+// Peach
 
-// saludarAlumno('Jairo')
-// saludarAlumno('Matias')
-// saludarAlumno('Fatima')
-// saludar()
-// saludar()
+let personajes = ['Mario','Luigi','Bowser','Toad','Peach',"Yoshi"]
 
+console.log(personajes)
 
-// function transportar(origen, destino){
-//     console.log("saliendo de " +origen)
-//     console.log("moviendome")
-//     console.log("llegando a " +destino)
-// }
+console.log("Quemado")
+console.log(personajes[0])
+console.log(personajes[1])
+console.log(personajes[2])
+console.log(personajes[3])
+console.log(personajes[4])
 
-// transportar("casa","gym")
-// transportar("gym","ofi")
-// transportar("ofi","casa")
-
-// transportar()
-// transportar()
-// transportar()
-// transportar()
-
-// function suma(num1,num2){
-//     // console.log('haciendo suma')
-//     // console.log(num1 + num2)
-//     return num1 + num2
-//     num1 = 8
-// }
-
-// console.log(suma(5,7))
-
-
-// function consumo(edad){
-//     if( edad >= 18){
-//         return "Puedes beber"
-//     }else{
-//         console.log('Me ven ')
-//         return "tienes que esperar"
-//     }
-// }
-
-
-// console.log(consumo(7))
-
-
-// parentesis ()
-// corchetes []
-// llaves {}
-/*
-LIMITacion del cliuen ete se peud eopitmizr peor el metiop xyz ya no funciona
-*/
-function calcularTax(income){
-    return income*0.34
+console.log("Con loop")
+for( i= personajes.length -1 ;i >= 0 ;i--){
+    // console.log(i)
+    console.log(personajes[i])
 }
 
-let resultado = calcularTax(30000)
 
-
-document.getElementById('amount').innerHTML = resultado
-
-
-// console.log('ANTES DE windows onlioad')
-// window.onload = function(){
-//     // su codigo 
-//     console.log('Se ejecuto windows onlioad')
-// }
-// console.log('DESPUES DE windows onlioad')
+// for(incializacion;condicion;actualizacion)
+// console.log ("luigi")
 
 
 
 
 
-// Funcion Declarativas
-saludoDeclarativo()
-function saludoDeclarativo(){
-    console.log("Hola saludoDeclarativo ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+console.log(numbers)
+
+
+//for(iniciaclizcion;condicion;actualizacion)
+//i = i + 3 ====> i+=3  i+=3
+// i = i + 1 ====>  i++
+for(i = 0;  i < numbers.length  ; i+=3){
+    console.log(numbers[i])
 }
 
-// Funcion Expresión
-const saludoExpresion = function(){
-    console.log("Hola saludoExpresion ")    
+
+
+let personajesBuenos = []
+
+for(i=0; i < personajes.length; i++){
+    console.log(personajes[i])
+    if( personajes[i] !== 'Bowser'){
+        personajesBuenos.push(personajes[i])
+    }
+    
 }
-saludoExpresion()
+    
+console.log(personajesBuenos)
 
 
-// Función Flecha  Arrow
-const saludoArrow = ()=> {
-    console.log("Hola saludoArrow ")    
+console.log(personajes.filter( (personaje)=> personaje != 'Bowser' ))
+
+console.log( numbers.filter( (numero)=> numero != 7 ))
+console.log( numbers.filter( (numero)=> numero > 7 ))
+console.log( numbers.filter( (numero)=> numero < 15 ))
+console.log( numbers.filter( (numero)=> numero > 7 & numero < 15 ))
+console.log( numbers.filter( (numero)=> numero % 3 == 0 ))
+console.log(numbers)
+
+
+let newNumbers = []
+
+for(i=0; i < numbers.length; i++){
+    newNumbers.push(numbers[i]* 4)
 }
-saludoArrow()
+
+console.log(numbers)
+console.log(newNumbers)
 
 
+// let mapNumbers = numbers.map( (item)=> item * 4 )
+console.log(numbers.map( (item)=> item * 4 ))
+console.log(numbers)
 
-///// REFACTORING /////
 
-// PSEUDOCODIGO
-// desarrollar un generador de excusas de Quiem, Accion, Cuando , Que
-// 1. modificar el htmlo desde js
-// 2. tomar un array de Quiem y obtene run elemento aleaotrio
-// 3. tomar un array de Accion y obtene run elemento aleaotrio
-// 4. tomar un array de Cuando y obtene run elemento aleaotrio
-// 5. tomar un array de Que y obtene run elemento aleaotrio
-// 6. concatenar los elmentos aleatorios
-// 7. asignar la concatenacion a la excusa en html
-// 8. agregar exuca a html
+console.log(personajes)
+console.log(personajes.map( (personaje)=> personaje + "s"))
+
+
+personajes.forEach( (character)=>{
+    console.log(character)
+})
+
+let j = 20
+do{
+    console.log(j);
+    j++;
+}while(j < 10)
+
