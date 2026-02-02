732299963a7bfb4982ac9ee8d2e5b605a2efc9ce
diego <diegozaratean@gmail.com>
2026-02-02 19:01:40 +0000

    finish first class dom

--- main.js
+++ main.js
@@ -1,48 +1,75 @@
 console.log('hola desde main js')
 
-// Hacer una app que calcule el impuesto si ganas menos de de 1000 debes imprimir "Debes pagar el 5%"
-// si ganas mas de 1000 debes imprimir "Debes pagar el 10%"
+const persona = {
+    nombre:"ulyses",
+    apellido:"odiseo",
+    edad:200,
+    skills:['js','python','rect'],
+    programar:  function(){
+        console.log("Estoy programando")
+    },
+    hijo: {
+        nombre: "arturo"
+    }
+}
+
+const myArray = ['ulyses','odiseo',200]
 
 
-// PSEUDOCODIGO
-// DONE : defnir mi variable de salario
-let salario = 500
-// DONE : comparar salario contra 1000
-// if(condicion){
-//     si es ciertyo que hago
-// }else{
-//     se no es cierto que hago
-// }
 
 
-// let mensaje = ''
-// if(salario < 1000){
-//     // debes imprimir el 5%
-//     // DONE : imprimir "Debes pagar el 5%"
-//     mensaje = "Debes pagar el 5%"
-// }else{
-//     // debes impriomir el 10%
-//     // DONE : imprimir "Debes pagar el 10%"
-//     mensaje = "Debes pagar el 10%"
-// }
 
 
-// Ternary operator
-// if(condicion){
-    //     si es ciertyo que hago
-    // }else{
-        //     se falso que hago
-        // }
-        // (condicion) ? si es ciertyo que hago: se falso que hago
-let mensaje =  salario < 1000 ? "Debes pagar el 5%" : "Debes pagar el 10%"
-        
-console.log(mensaje)
-// (condicion) ? si es ciertyo que hago: 
-//            (condicion) ? si es ciertyo que hago: se falso que hago
 
-// document.getElementById("content").innerHTML = mensaje
 
-document.getElementById("content").insertAdjacentHTML(
-    "beforeend",
-    `<p> mensaje  ${mensaje} </p>`
-)
+
+
+
+
+
+
+
+// Agregar una función que cambie el color de fondo del tituloa rojo y 
+// el texto del titulo a "nuevo titulo"
+
+// PSEUDOCDOIGO
+// crear funcion
+function cambiarTitulo(){
+    console.log("cambiarTitulo")
+}
+cambiarTitulo()
+// obtener el titulo
+let titulo = document.getElementById('title') 
+
+// cambiar color de fondo
+titulo.style.backgroundColor = 'red' 
+// cmabiar texto
+titulo.textContent = 'nuevo titulo '
+
+
+
+
+
+
+
+
+
+
+
+
+// Objeto Huron: color,comportamiento, nombre
+// correr "Estoy corriendo"
+const huron = {
+    nombre: "Pepito",
+    color: "negro y blanco",
+    comportamiento: "agresivo",
+    correr: function(){
+        console.log("Estoy corriendo")
+    }
+} 
+
+
+
+
+
+
