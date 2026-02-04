7589915ce69f639127e34dc6e8fcf87572dd7795
diego <diegozaratean@gmail.com>
2026-02-04 19:21:09 +0000

    js dice class 1

--- main.js
+++ main.js
@@ -42,9 +42,99 @@ cambiarTitulo()
 let titulo = document.getElementById('title') 
 
 // cambiar color de fondo
-titulo.style.backgroundColor = 'red' 
+// titulo.style.backgroundColor = 'red' 
 // cmabiar texto
-titulo.textContent = 'nuevo titulo '
+// titulo.textContent = 'nuevo titulo '
+
+
+
+
+
+// PSEUDOCDOIGO
+// realiza un programa que cambie de forma aleatoria el color
+//  y el texto del titulo 
+function changeTitle(){
+    const colors = ['blue','red','purple','green',"aqua"]
+    const students = ['MAtias','jairo','Ramon',"Mark"]
+    let randomColor = colors[Math.floor(Math.random() * colors.length)]
+    document.getElementById('title').style.color = randomColor
+    let randomStudent = students[Math.floor(Math.random() * students.length)]
+    document.getElementById("title").textContent = randomStudent
+}
+
+changeTitle()
+
+
+
+
+
+
+
+
+
+
+// PSEUDOCDOIGO
+// realiza un programa que lance dos dados y los muestre al usuario en dos parrafos
+// DONE: agregar funcion
+function lanzarDados(){
+    console.log('lanzarDados')
+    let dado1 = Math.floor(Math.random() * 6) + 1
+    
+    // Math.random()   ===> 0 a 0.999
+    // Math.random() * 6   ===> 0 a 5.999
+    // Math.floor(Math.random() * 6)  ===> 0 a 5
+    // Math.floor(Math.random() * 6) + 1  ===> 1 a 6
+    let dado2 = Math.ceil(Math.random() * 6) 
+    // Math.random()   ===> 0 a 0.999
+    // Math.random() * 6   ===> 0 a 5.999
+    // Math.ceil(Math.random() * 6)  ===> posiblemente o y casi siempre 1 a 6
+    dado2 = (dado2 == 0) ? 1 : dado2
+
+    document.querySelector("#dice1").textContent = dado1
+    document.querySelector("#dice2").textContent = dado2
+    
+    console.log(dado1)
+    console.log(dado2)
+
+    console.log('la suma de los dados es:' + (dado1 + dado2))
+    // DONE: agregar parrafos al html
+    // DONE: agregar boton al html
+    // vincular funcion con boton
+}
+// DONE: ejecutar la funcion
+lanzarDados()
+// DONE: definir variables para numeros de dados
+//
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
